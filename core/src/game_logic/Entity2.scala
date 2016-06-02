a8a8e5752fe26ae8ca665e1a9f0054c8bcac7e17
Emily <exwu.94@gmail.com>
2016-06-02 19:09:21 -0400

    some experiments in the ecs model

---
+++ core/src/game_logic/Entity2.scala
@@ -0,0 +1,8 @@
+package game_logic
+
+/**
+  * Created by emily on 6/2/16.
+  */
+class Entity2 {
+
+}
---
+++ core/src/game_logic/Workstation.scala
@@ -0,0 +1,8 @@
+package game_logic
+
+/**
+  * Created by emily on 5/31/16.
+  */
+class Workstation {
+
+}
